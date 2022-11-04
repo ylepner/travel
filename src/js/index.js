382c75d2fc16404d34d2ec3f9c039afb37c708a3
Yulia Lepner <ylepner@gmail.com>
2022-11-04 21:50:51 +0100

    feat: update slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,38 @@
+let centerImageNumber = 1;
+let countries = ['SPAIN', 'JAPAN', 'USA']
+const slider = document.querySelector('.slider__images')
+
+
 window.onload = function () {
-  console.log(checkIfMobileScreen());
-  addImageHandler();
+  if (checkIfMobileScreen()) {
+    addImageHandlerMobile();
+  } else {
+    addImageHandlerDesktop();
+  }
 }
 
 const checkIfMobileScreen = () => {
   return window.innerWidth <= 390
+}
+
+const addImageHandlerDesktop = () => {
+  document.querySelector('.left-direction').addEventListener('click', (e) => {
+    slide(-1)
+  })
+  document.querySelector('.right-direction').addEventListener('click', (e) => {
+    slide(1)
+  })
+}
+
+const addImageHandlerMobile = () => {
+  document.querySelector('.slider__switcher').addEventListener('click', (e) => {
+    if (e.target.classList.contains('switch__button_left')) {
+      slideImageMobile('left')
+    }
+    if (e.target.classList.contains('switch__button_right')) {
+      slideImageMobile('right')
+    }
+  })
 }
 
 const removeSelectedButtons = () => {
@@ -20,29 +48,47 @@ const addClassToSlideNumber = (buttonClass) => {
   document.querySelector(`${buttonClass}`).classList.add('slide-number-identifier_selected');
 }
 
-const slideImage = (el) => {
-  console.log(el.classList.contains('image_1'))
-  const slider = document.querySelector('.slider__images')
-  if (['round-1', 'image_1'].some(cl => el.classList.contains(cl))) {
+const slide = (direction) => {
+  centerImageNumber = mod((centerImageNumber + (direction)), 3) // 2
+  slider.classList.add('animating')
+  slider.style.transform = `translateX(${50 * (direction)}%)`;
+  if (direction < 0) {
     slider.style.transform = 'translateX(50%)';
-    removeSelectedButtons();
-    addClassToSlideNumber('.round-1');
+  } else {
+    slider.style.transform = 'translateX(-50%)'
   }
-  if (['round-2', 'image_2'].some(cl => el.classList.contains(cl))) {
-    slider.style.transform = 'translateX(0%)';
-    removeSelectedButtons();
-    addClassToSlideNumber('.round-2');
-  }
-  if (['round-3', 'image_3'].some(cl => el.classList.contains(cl))) {
-    slider.style.transform = 'translateX(-50%)';
-    removeSelectedButtons();
-    addClassToSlideNumber('.round-3');
-  }
+  setTimeout(() => {
+    slider.classList.remove('animating')
+    slider.style.transform = 'translate(0%)'
+    replaceCards(direction)
+  }, 2000)
+
 }
 
-const addImageHandler = () => {
-  debugger
-  document.querySelector('.slider__images').addEventListener('click', (e) => {
-    slideImage(e.target)
-  })
+const replaceCards = (direction) => {
+  centerImageNumber
+  const newSlideNumber = mod((centerImageNumber + direction * 2), 3)
+  const newSlideDiv = document.createElement('div')
+  newSlideDiv.classList.add('slider__image')
+  const img = document.createElement('img')
+  img.src = `/assets/img/dest-${newSlideNumber}.jpg`
+  img.className = `image image_${newSlideNumber}`
+  newSlideDiv.appendChild(img)
+  const text = document.createElement('div')
+  text.className = 'image__text'
+  text.innerHTML = `${countries[newSlideNumber]}`
+  newSlideDiv.appendChild(text)
+  const images = document.querySelectorAll('.slider__image')
+  if (direction < 0) {
+    slider.prepend(newSlideDiv)
+    slider.removeChild(images[images.length - 1])
+  } else {
+    slider.appendChild(newSlideDiv)
+    slider.removeChild(images[0])
+  }
+
+}
+
+const mod = (m, n) => {
+  return (m + n) % n
 }
